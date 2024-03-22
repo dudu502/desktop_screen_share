8b2380b77bf84987018c5fbde42ac159fa0c5423
emilyeffie <162259964+emilyeffie@users.noreply.github.com>
2024-03-22 17:46:43 +0800

    Copy dll file from DesktopHost directory to DesktopViewer directory

--- DesktopViewer/Assets/ThinkViewer/Plugins/auto_copy_dlls.bat
+++ DesktopViewer/Assets/ThinkViewer/Plugins/auto_copy_dlls.bat
@@ -1,1 +1,11 @@
-copy D:\dudu502\desktop_screen_share\DesktopHost\Common\bin\Debug\netstandard2.0\Common.dll D:\dudu502\desktop_screen_share\DesktopViewer\Assets\ThinkViewer\Plugins\Common.dll
+REM copy D:\dudu502\desktop_screen_share\DesktopHost\Common\bin\Debug\netstandard2.0\Common.dll D:\dudu502\desktop_screen_share\DesktopViewer\Assets\ThinkViewer\Plugins\Common.dll
+REM @echo off
+setlocal EnableDelayedExpansion
+
+set "currentDirectory=%~dp0"
+set "subs=DesktopViewer"
+
+%~dp0\cpdll.exe  !currentDirectory!  !subs!
+endlocal
+
+cmd
