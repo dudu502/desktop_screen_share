f6c9e1728f228581ab32746942be16e88460284d
emilyeffie <162259964+emilyeffie@users.noreply.github.com>
2024-03-25 14:04:14 +0800

    Add argument filename when executing file copy

--- DesktopViewer/Assets/ThinkViewer/Plugins/auto_copy_dlls.bat
+++ DesktopViewer/Assets/ThinkViewer/Plugins/auto_copy_dlls.bat
@@ -1,11 +1,12 @@
 REM copy D:\dudu502\desktop_screen_share\DesktopHost\Common\bin\Debug\netstandard2.0\Common.dll D:\dudu502\desktop_screen_share\DesktopViewer\Assets\ThinkViewer\Plugins\Common.dll
-REM @echo off
+@echo off
 setlocal EnableDelayedExpansion
 
 set "currentDirectory=%~dp0"
 set "subs=DesktopViewer"
+set "filename=Common.dll"
 
-%~dp0\cpdll.exe  !currentDirectory!  !subs!
+%~dp0\cpdll.exe  !currentDirectory! !subs! !filename!
 endlocal
 
-cmd
+timeout /t 10
